8c65d9c6b691e089b66c6de193f251c3fefc7cb6
I352778 <c16492454@mydit.ie>
2020-05-13 14:33:49 +0100

    Added tests to see if creation of surveys work

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Controller {
 
 
@@ -10,6 +12,20 @@ public class Controller {
         Survey createSurvey = new Survey("Quality");
 
         return createSurvey.getName();
+    }
+
+    public String surveyQuestions()
+    {
+        //Creation of question object
+        Question one = new Question("Customer Service");
+        //Collection to represent questions
+        ArrayList<Question> questions = new ArrayList<Question>();
+        //adding question to list
+        questions.add(one);
+
+        Survey s = new Survey("My Questions" ,questions);
+        
+        return "true";
     }
 
 }
--- src/TestJunit.java
+++ src/TestJunit.java
@@ -1,14 +1,16 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertThat;
 
 
 public class TestJunit {
+    ArrayList<Survey> surveys = new ArrayList<>();
 
-    List<Survey> list;
 
     @Test
     public void testAdd() {
@@ -16,6 +18,32 @@ public class TestJunit {
         Controller controller = new Controller();
         assertEquals("Quality", controller.CreateSurvey());
 
+    }
+
+    @Test
+    public void Creationofsurvey()
+    {
+
+        Survey firstsurvey = new Survey();
+
+        surveys.add(firstsurvey);
+        assertTrue("first survey belongs to survey",surveys.contains(firstsurvey));
+    }
+
+    @Test
+    public void createSurveyQuestions()
+    {
+        //Creation of question object
+        Question firstquestion = new Question("Quality");
+        //Collection to represent questions
+        ArrayList<Question> questions = new ArrayList<Question>();
+        //adding question to list
+        questions.add(firstquestion);
+
+        Survey firstquestionsurvey = new Survey("Quality Questions" ,questions);
+        surveys.add(firstquestionsurvey);
+
+        assertTrue("Survey belongs to survey",surveys.contains(firstquestionsurvey));
     }
 
 
